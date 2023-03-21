71f7dc843348bf19df7b803ad74a08ec7cd0897b
Lucas Oliveira <olivmath@protonmail.com>
2023-03-20 22:56:57 -0300

    add: logger module

--- src/logger/mod.rs
+++ src/logger/mod.rs
@@ -3,6 +3,39 @@ use std::error::Error;
 use term_size::dimensions;
 use textwrap::fill;
 
+
+/// Logs the given text to stdout, wrapped and formatted according to the terminal width.
+///
+/// The input text is wrapped based on the terminal width, and the wrapped text is printed
+/// with a green line above and below. If the terminal width cannot be determined, a default
+/// width of 80 characters is used.
+///
+/// # Arguments
+///
+/// * `text` - The text to be logged to stdout.
+///
+/// # Returns
+///
+/// * `Result<(), Box<dyn Error + Send + Sync>>` - Returns an empty `Result` on successful logging.
+///   In case of any error, it returns an `Error` trait object.
+///
+/// # Errors
+///
+/// This function might return an error if there's an issue with stdout.
+///
+/// # Example
+///
+/// ```
+/// stdout_log("This is a sample log message.").await.unwrap();
+/// ```
+/// 
+/// # Result in Terminal
+/// 
+/// ```
+/// // ------------------------------------------------------------
+/// // This is a sample log message.
+/// // ------------------------------------------------------------
+/// ```
 pub async fn stdout_log(text: &str) -> Result<(), Box<dyn Error + Send + Sync>> {
     let width = dimensions().unwrap_or((80, 24)).0; // obtém a largura atual do terminal ou usa 80 como padrão
     let wrapped_text = fill(text, width); // quebra a string em várias linhas de acordo com a largura
