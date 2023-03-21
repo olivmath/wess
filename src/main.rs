29859b31a800fb9ac70a971029fcb6c5d5e4131c
Lucas Oliveira <olivmath@protonmail.com>
2023-03-20 23:13:25 -0300

    update: log messages

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,14 @@ use std::error::Error;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    stdout_log("💽 start RocksDB data base").await?;
+    stdout_log("💽 Start RocksDB data base").await?;
     let db = RocksDB::new();
-    stdout_log("🚿 Create channels for HTTP requests").await?;
 
-    stdout_log("👷 Criar um conjunto de threads para executar as tarefas em segundo plano").await?;
-    stdout_log("📡 Enviar as tarefas recebidas para o canal de tarefas dos runners").await?;
-    stdout_log("🛰️ Configurar o servidor usando o framework Tide").await?;
+    stdout_log("🚿 Create channels for HTTP requests").await?;
+    stdout_log("👷 Create a threadset to run the tasks in the background").await?;
+    stdout_log("📡 Send the received tasks to the runners' tasks channel").await?;
+
+    stdout_log("🛰️  Run server on `http://127.0.0.1:3000`").await?;
     let wess = WessServer::new(db);
     wess.run("127.0.0.1:3000").await?;
 
