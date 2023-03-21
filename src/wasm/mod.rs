ffbfb80ba2125c9abd940d360647da0330df8a99
Lucas Oliveira <olivmath@protonmail.com>
2023-03-21 09:29:23 -0300

    update: move code wasm.rs to mod.rs

--- src/wasm/mod.rs
+++ src/wasm/mod.rs
@@ -1,2 +1,26 @@
 pub mod signature;
-pub mod wasm;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq)]
+pub struct WasmFnArgs {
+    pub value: serde_json::Value,
+    pub name: String,
+    #[serde(rename = "type")]
+    pub arg_type: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq)]
+pub struct WasmMetadata {
+    pub owner: Vec<u8>,
+    pub signature: Vec<u8>,
+    pub id: u32,
+    pub fn_main: String,
+    pub args: Vec<WasmFnArgs>,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq)]
+pub struct Wasm {
+    pub wasm: Vec<u8>,
+    pub metadata: WasmMetadata,
+}
