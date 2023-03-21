5b3cde707d4d33329c489726b9f32cba71cd2000
Lucas Oliveira <olivmath@protonmail.com>
2023-03-20 22:06:41 -0300

    add: wasm module

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+mod database;
 mod logger;
+mod wasm;
 
-use std::error::Error;
 use logger::stdout_log;
+use std::error::Error;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    stdout_log("💽 Configurar o banco de dados RocksDB").await?;
+    stdout_log("💽 start RocksDB data base").await?;
     stdout_log("🚿 Criar um canal de tarefas para receber as solicitações HTTP").await?;
+    
+
     stdout_log("👷 Criar um conjunto de threads para executar as tarefas em segundo plano").await?;
     stdout_log("📡 Enviar as tarefas recebidas para o canal de tarefas dos runners").await?;
     stdout_log("🛰️ Configurar o servidor usando o framework Tide").await?;
