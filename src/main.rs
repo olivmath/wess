426637d1ec7853639f53e7eff3b17fe904a81b8f
Lucas Oliveira <olivmath@protonmail.com>
2023-03-21 09:35:45 -0300

    update: main to new api

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod logger;
 pub mod server;
 mod wasm;
 
-use database::rocksdb::RocksDB;
+use database::RocksDB;
 use logger::stdout_log;
 use server::WessServer;
 use std::error::Error;
@@ -11,14 +11,14 @@ use std::error::Error;
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     stdout_log("💽 Start RocksDB data base").await?;
-    let db = RocksDB::new();
+    let _db = RocksDB::new();
 
     stdout_log("🚿 Create channels for HTTP requests").await?;
     stdout_log("👷 Create a threadset to run the tasks in the background").await?;
     stdout_log("📡 Send the received tasks to the runners' tasks channel").await?;
 
     stdout_log("🛰️  Run server on `http://127.0.0.1:3000`").await?;
-    let wess = WessServer::new(db);
+    let wess = WessServer::new();
     wess.run("127.0.0.1:3000").await?;
 
     Ok(())
