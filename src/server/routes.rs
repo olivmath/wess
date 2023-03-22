05dfa5cc268489f24153965b4c56f9ab4d902efa
Lucas Oliveira <olivmath@protonmail.com>
2023-03-22 09:15:28 -0300

    update: routes functions to job maker-2

--- src/server/routes.rs
+++ src/server/routes.rs
@@ -5,13 +5,11 @@ pub fn get_wasm(_req: Request<()>) -> Result<Response, Error> {
     Ok(Response::builder(StatusCode::Ok).body("get wasm").build())
 }
 
-pub fn add_new_wasm(_req: Request<()>) -> Result<Response, Error> {
-    Ok(Response::builder(StatusCode::Ok).body("new wasm").build())
-}
+async fn send_to_runner(wasm_job: WasmJob, tx: Sender<WasmJob>) -> Result<Response, Error> {
+    tx.send(wasm_job.clone()).await.unwrap();
 
-pub fn update_wasm(_req: Request<()>) -> Result<Response, Error> {
-    Ok(Response::builder(StatusCode::Ok)
-        .body("update wasm")
+    Ok(Response::builder(StatusCode::Created)
+        .body(json!({ "id": wasm_job.id }))
         .build())
 }
 
