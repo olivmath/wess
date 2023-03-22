f2bc3ffa86d0f00e854957c51fe9fbc273c218f3
Lucas Oliveira <olivmath@protonmail.com>
2023-03-22 09:15:10 -0300

    update: routes functions to job maker

--- src/server/routes.rs
+++ src/server/routes.rs
@@ -15,8 +15,20 @@ pub fn update_wasm(_req: Request<()>) -> Result<Response, Error> {
         .build())
 }
 
-pub fn remove_wasm(_req: Request<()>) -> Result<Response, Error> {
-    Ok(Response::builder(StatusCode::Ok)
-        .body("delete wasm")
-        .build())
+pub async fn job_maker(mut req: Request<AppState>, job_type: JobType) -> Result<Response, Error> {
+    if let Ok(wasm_req) = req.body_json::<WasmRequest>().await {
+        send_to_runner(
+            WasmJob {
+                job_type,
+                id: "0x22ff".to_string(), // TODO hash the .wasm with SHA256
+                wasm_req,
+            },
+            req.state().tx.clone(),
+        )
+        .await
+    } else {
+        Ok(Response::builder(StatusCode::NotFound)
+            .body(json!({"message": "invalid json"}))
+            .build())
+    }
 }
