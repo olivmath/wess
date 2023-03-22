80e30c76112c7745a4e326818aa3cc0ba09dc443
Lucas Oliveira <olivmath@protonmail.com>
2023-03-22 09:16:11 -0300

    fix: main with new api

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod database;
 mod logger;
-pub mod server;
+mod runner;
+mod server;
 mod wasm;
 
 use database::RocksDB;
 use logger::{clear_terminal_with, stdout_log};
-use server::WessServer;
+use server::{request::WasmJob, WessServer};
 use std::error::Error;
+use tokio::sync::mpsc;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
@@ -16,12 +18,14 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let _db = RocksDB::new();
 
     stdout_log("🚿 Create channels for HTTP requests").await?;
-    stdout_log("👷 Create a threadset to run the tasks in the background").await?;
-    stdout_log("📡 Send the received tasks to the runners' tasks channel").await?;
+    let (tx, rx) = mpsc::channel::<WasmJob>(100);
 
     stdout_log("🛰️  Run server on `http://127.0.0.1:3000`").await?;
-    let wess = WessServer::new();
-    wess.run("127.0.0.1:3000").await?;
+    let wess = WessServer::new(tx);
+    wess.run("127.0.0.1:3000").await;
+
+    stdout_log("👷 Create a threadset to run the tasks in the background").await?;
+    stdout_log("📡 Send the received tasks to the runners' tasks channel").await?;
 
     Ok(())
 }
