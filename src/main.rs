a99d92c2bf885e1220f5748202f0d8abaf7149c2
Lucas Oliveira <olivmath@protonmail.com>
2023-03-24 17:53:27 -0300

    fix(main): async task with tokio

--- src/main.rs
+++ src/main.rs
@@ -15,22 +15,39 @@ use tokio::{
     try_join,
 };
 
-#[async_std::main]
+#[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     clear_terminal_with("");
 
     stdout_log("💽 Start RocksDB data base").await?;
-    let _db = RocksDB::new();
+    let db = RocksDB::new();
 
     stdout_log("🚿 Create channels for HTTP requests").await?;
     let (tx, rx) = mpsc::channel::<Job>(100);
 
-    stdout_log("🛰️  Run server on `http://127.0.0.1:3000`").await?;
-    let wess = WessServer::new(tx);
-    wess.run("127.0.0.1:3000").await;
-
     stdout_log("👷 Create a threadset to run the tasks in the background").await?;
+    let runner = Arc::new(Mutex::new(Runner::new(rx, db)));
+
+    stdout_log("🛰️  Run server on `http://127.0.0.1:3000`").await?;
+    let wess = Arc::new(Mutex::new(WessServer::new(tx)));
+
     stdout_log("📡 Send the received tasks to the runners' tasks channel").await?;
 
+    let runner_task = {
+        let runner = Arc::clone(&runner);
+        tokio::spawn(async move {
+            runner.lock().await.run().await;
+        })
+    };
+
+    let server_task = {
+        let wess = Arc::clone(&wess);
+        tokio::spawn(async move {
+            let wess_instance = wess.lock().await.clone();
+            wess_instance.run("127.0.0.1:3000").await.unwrap();
+        })
+    };
+
+    try_join!(server_task, runner_task)?;
     Ok(())
 }
