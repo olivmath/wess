8fe80a048284a105d56dda6e041e8ff560e3f43a
Lucas Oliveira <olivmath@protonmail.com>
2023-03-26 23:39:38 -0300

    remove(server): this code will convert into a module

--- src/server/request.rs
+++
@@ -1,9 +0,0 @@
-use crate::wasm::WasmFnArgs;
-use serde::Deserialize;
-
-#[derive(Deserialize, Debug, Clone)]
-pub struct WasmRequest {
-    pub wasm: Vec<u8>,
-    pub run_function: String,
-    pub args: Vec<Option<WasmFnArgs>>,
-}
