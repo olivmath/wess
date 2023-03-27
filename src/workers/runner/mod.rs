b0f456b4f1b79c5a560808423a2c492b015dd121
Lucas Oliveira <olivmath@protonmail.com>
2023-03-26 23:53:30 -0300

    add(workers/runner): this is a async executor for read wasm function from db and run this function with the params passed by client in server requests

---
+++ src/workers/runner/mod.rs
@@ -0,0 +1,59 @@
+pub mod models;
+mod wasm;
+
+use self::models::{RunJob, RunResponse, RunnerError};
+use crate::{
+    database::{models::WasmFn, RocksDB},
+    server::request::RunRequest,
+};
+use std::sync::Arc;
+use tokio::sync::{
+    mpsc::{self, Receiver, Sender},
+    Mutex,
+};
+
+pub struct Runner {
+    rx: Receiver<RunJob>,
+    db: RocksDB,
+}
+
+impl Runner {
+    pub fn new(db: RocksDB) -> (Sender<RunJob>, Arc<Mutex<Runner>>) {
+        let (tx, rx) = mpsc::channel::<RunJob>(100);
+        (tx, Arc::new(Mutex::new(Runner { rx, db })))
+    }
+
+    pub async fn run(&mut self) {
+        while let Some(job) = self.rx.recv().await {
+            //
+            let responder = job.responder;
+            let args = job.args;
+            let id = job.id;
+            //
+            match self.db.get(id.as_str()) {
+                Some(wasm_fn) => match self.run_function(&wasm_fn, &args).await {
+                    Ok(result) => {
+                        tokio::spawn(async move { responder.send(result) });
+                    }
+                    Err(e) => {
+                        tokio::spawn(async move { responder.send(RunResponse::Fail(e)) });
+                    }
+                },
+                None => {
+                    tokio::spawn(async move {
+                        responder.send(RunResponse::fail(RunnerError::WasmNotFound))
+                    });
+                }
+            };
+        }
+    }
+
+    pub async fn run_function(
+        &mut self,
+        _wasm_fn: &WasmFn,
+        _args: &RunRequest,
+    ) -> Result<RunResponse, RunnerError> {
+        //
+        Ok(RunResponse::new("foi".into()))
+    }
+}
---
+++ src/workers/runner/models.rs
@@ -0,0 +1,47 @@
+use crate::server::request::RunRequest;
+use serde::Serialize;
+use tokio::sync::oneshot::Sender;
+
+/// Run Job Type
+#[derive(Debug)]
+pub struct RunJob {
+    pub responder: Sender<RunResponse>,
+    pub args: RunRequest,
+    pub id: String,
+}
+
+impl RunJob {
+    pub fn new(responder: Sender<RunResponse>, args: RunRequest, id: String) -> Self {
+        Self {
+            responder,
+            args,
+            id,
+        }
+    }
+}
+
+/// # Run Response Type
+#[derive(Debug, Serialize)]
+pub enum RunResponse {
+    Success(String),
+    Fail(RunnerError),
+}
+
+impl RunResponse {
+    /// # Success: [`String`]
+    pub fn new(r: String) -> Self {
+        RunResponse::Success(r)
+    }
+
+    /// # Fail: [`RunnerError`]
+    pub fn fail(msg: RunnerError) -> Self {
+        RunResponse::Fail(msg)
+    }
+}
+
+#[derive(Serialize, Debug)]
+pub enum RunnerError {
+    Execution(String),
+    Unknown(String),
+    WasmNotFound,
+}
