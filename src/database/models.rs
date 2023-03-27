0666b79d5225c2c52adb60d8eb5e3777d55dfb66
Lucas Oliveira <olivmath@protonmail.com>
2023-03-26 22:51:54 -0300

    update(database): these are the types I talked about 2 commits ago, the wasm fn structure contains all the information needed to execute the functions and can be extended to compose functio status in the future

---
+++ src/database/models.rs
@@ -0,0 +1,88 @@
+use serde::{Deserialize, Serialize};
+
+/// Represents WebAssembly bytecode.
+pub type Wasm = Vec<u8>;
+
+/// Represents an argument for a WebAssembly function.
+#[derive(Serialize, Deserialize, Debug, Default, Clone, PartialEq)]
+pub struct FnTypeArg {
+    /// The name of the argument.
+    pub name: String,
+    /// The type of the argument.
+    pub arg_type: String,
+}
+
+impl FnTypeArg {
+    /// # Creates a new instance of the `FnTypeArg` structure.
+    ///
+    /// ## Arguments
+    ///
+    /// * `name` - A string slice that represents the name of the argument.
+    /// * `arg_type` - A string slice that represents the type of the argument.
+    ///
+    /// ## Returns
+    ///
+    /// * An instance of `FnTypeArg`.
+    pub fn new(name: String, arg_type: String) -> Self {
+        Self { name, arg_type }
+    }
+}
+
+/// Represents metadata associated with a WebAssembly function.
+#[derive(Serialize, Deserialize, Debug, Default, Clone, PartialEq)]
+pub struct WasmMetadata {
+    // pub owner: Vec<u8>,
+    // pub signature: Vec<u8>,
+    /// The name of the function.
+    pub func: String,
+    /// The return type of the function.
+    pub func_return_type: String,
+    /// A vector of function argument types.
+    pub args: Vec<Option<FnTypeArg>>,
+}
+
+impl WasmMetadata {
+    /// # Creates a new instance of the `WasmMetadata` structure.
+    ///
+    /// ## Arguments
+    ///
+    /// * `func` - A string slice that represents the name of the function.
+    /// * `func_return_type` - A string slice that represents the return type of the function.
+    /// * `args` - A vector of `Option<FnTypeArg>` objects that represent the function argument types.
+    ///
+    /// ## Returns
+    ///
+    /// * An instance of `WasmMetadata`.
+    pub fn new(func: String, func_return_type: String, args: Vec<Option<FnTypeArg>>) -> Self {
+        Self {
+            func,
+            func_return_type,
+            args,
+        }
+    }
+}
+
+/// Represents a WebAssembly function.
+#[derive(Serialize, Deserialize, Debug, Default, Clone, PartialEq)]
+pub struct WasmFn {
+    /// The WebAssembly bytecode.
+    pub wasm: Wasm,
+    /// The metadata associated with the function.
+    pub metadata: WasmMetadata,
+}
+
+impl WasmFn {
+    /// # Creates a new instance of the `WasmFn` structure.
+    ///
+    /// ## Arguments
+    ///
+    /// * `wasm` - A `Wasm` object that represents the WebAssembly bytecode.
+    /// * `metadata` - A `WasmMetadata` object that represents the metadata associated with the function.
+    ///
+    /// ## Returns
+    ///
+    /// * An instance of `WasmFn`.
+    pub fn new(wasm: Wasm, metadata: WasmMetadata) -> Self {
+        Self { wasm, metadata }
+    }
+}
