a0c0a133bd8ee2c32a3a15cf98dde657d94653ec
Lucas Oliveira <olivmath@protonmail.com>
2023-03-26 22:49:55 -0300

    update(database): unknown to allow strings, and more docs

--- src/database/errors.rs
+++ src/database/errors.rs
@@ -1,14 +1,19 @@
+/// An enum representing possible errors that can occur while interacting with RocksDB.
+#[derive(Debug)]
 pub enum RocksDBError {
-    Unknown,
+    /// An unknown error occurred.
+    Unknown(String),
+    /// The requested key was not found in the database.
     NotFound,
-    // ..
+    // ...
 }
 
 impl ToString for RocksDBError {
+    /// Converts the error into a string representation.
     fn to_string(&self) -> String {
         match self {
             RocksDBError::NotFound => "NotFound".to_string(),
-            RocksDBError::Unknown => "Unknown".to_string(),
+            RocksDBError::Unknown(e) => format!("Unknown {}", e),
             // ...
         }
     }
