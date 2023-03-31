ce9fb129d1f5649757430e447786cd8755f4e723
Lucas Oliveira <olivmath@protonmail.com>
2023-03-31 10:25:13 -0300

    add(metrics): fmt

--- src/metrics/constants.rs
+++ src/metrics/constants.rs
@@ -1,11 +1,16 @@
-#![rustfmt::skip]
 use lazy_static::lazy_static;
 use prometheus::{
     register_counter, register_histogram, register_int_counter, register_int_gauge, Counter,
     Histogram, IntCounter, IntGauge,
 };
 
+#[rustfmt::skip]
 lazy_static! {
+    pub static ref CONCURRENT_CONNECTIONS: IntGauge = register_int_gauge!(
+        "wess_concurrent_connections",
+        "Number of concurrent connections"
+    ).unwrap();
+
     pub static ref HTTP_REQUESTS_TOTAL: Counter = register_counter!(
         "wess_http_requests_total",
         "Total number of HTTP requests"
@@ -44,10 +49,5 @@ lazy_static! {
     pub static ref DATABASE_SIZE: IntGauge = register_int_gauge!(
         "wess_database_size_bytes",
         "Size of the database in bytes"
-    ).unwrap();
-
-    pub static ref CONCURRENT_CONNECTIONS: IntGauge = register_int_gauge!(
-        "wess_concurrent_connections",
-        "Number of concurrent connections"
     ).unwrap();
 }
