0052b022096a9ab760f00d53f125776019d2fcec
Lucas Oliveira <olivmath@protonmail.com>
2023-04-03 23:29:54 -0300

    add(workers/writer): now write is responsable to talk with reader for alert about that a some data has been changed and must be removed from cache to avoid inconsistences

--- src/workers/writer/mod.rs
+++ src/workers/writer/mod.rs
@@ -13,13 +13,17 @@ pub mod models;
 use self::models::{WJob, WOps, WriterError};
 use crate::{config::CONFIG, database::RocksDB, logger};
 use std::sync::Arc;
-use tokio::sync::{
-    mpsc::{self, Receiver, Sender},
-    Mutex,
+use tokio::{
+    spawn,
+    sync::{
+        mpsc::{self, Receiver, Sender},
+        Mutex,
+    },
 };
 
 /// An async executor for writing data into the database.
 pub struct Writer {
+    tx: Sender<String>,
     rx: Receiver<WJob>,
     db: RocksDB,
 }
@@ -28,10 +32,17 @@ impl Writer {
     // # Creates a new instance of [`Writer`] with the given `db` instance.
     ///
     /// Returns a tuple containing a [`Sender<WJob>`] and an [`Arc<Mutex<Writer>>`] instance.
-    pub fn new(db: RocksDB) -> (Sender<WJob>, Arc<Mutex<Writer>>) {
+    pub fn new(db: RocksDB, tx_reader: Sender<String>) -> (Sender<WJob>, Arc<Mutex<Writer>>) {
         let channel_size = CONFIG.writer.channel_size;
         let (tx, rx) = mpsc::channel::<WJob>(channel_size);
-        (tx, Arc::new(Mutex::new(Writer { rx, db })))
+        (
+            tx,
+            Arc::new(Mutex::new(Writer {
+                tx: tx_reader,
+                rx,
+                db,
+            })),
+        )
     }
 
     /// # Runs the async executor for writing data into the database.
@@ -41,23 +52,30 @@ impl Writer {
             let job_type = job.wtype;
             let wasm_req = job.wreq;
             let id = job.id.as_str();
+            let send_reader = self.tx.clone();
             //
             match job_type {
                 WOps::Create => {
-                    if let Err(e) = self.db.add(id, wasm_req.to_wasm_fn().unwrap()) {
+                    if let Err(e) = self.db.add(id, wasm_req.unwrap().to_wasm_fn().unwrap()) {
                         logger::log_error(WriterError::Create(id.to_string(), e.to_string()));
-                    };
+                    }
                 }
-                WOps::Update => {
-                    if let Err(e) = self.db.upd(id, wasm_req.to_wasm_fn().unwrap()) {
+                WOps::Update => match self.db.upd(id, wasm_req.unwrap().to_wasm_fn().unwrap()) {
+                    Ok(id) => {
+                        spawn(async move { send_reader.send(id).await });
+                    }
+                    Err(e) => {
                         logger::log_error(WriterError::Update(id.to_string(), e.to_string()));
-                    };
-                }
-                WOps::Delete => {
-                    if let Err(e) = self.db.del(id) {
+                    }
+                },
+                WOps::Delete => match self.db.del(id) {
+                    Ok(id) => {
+                        spawn(async move { send_reader.send(id).await });
+                    }
+                    Err(e) => {
                         logger::log_error(WriterError::Delete(id.to_string(), e.to_string()));
-                    };
-                }
+                    }
+                },
             }
         }
     }
