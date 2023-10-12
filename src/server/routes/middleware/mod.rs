9e487588e6076b465b3549f71f057ce3b24ef874
Lucas Oliveira <olivmath@protonmail.com>
2023-10-12 01:48:21 -0300

    update: implement metrics in middleware

--- src/server/routes/middleware/mod.rs
+++ src/server/routes/middleware/mod.rs
@@ -1,7 +1,7 @@
 use std::time::Instant;
 
 use crate::metrics::constants::{
-    CONCURRENT_CONNECTIONS, HTTP_REQUESTS_TOTAL, HTTP_REQUEST_LATENCY,
+    CONCURRENT_CONNECTIONS, ERROR_COUNT, HTTP_REQUESTS_TOTAL, HTTP_REQUEST_LATENCY,
 };
 use tide::{Middleware, Next, Request, Result};
 
@@ -10,18 +10,23 @@ pub struct RequestMetricsMiddleware;
 #[tide::utils::async_trait]
 impl<State: Clone + Send + Sync + 'static> Middleware<State> for RequestMetricsMiddleware {
     async fn handle(&self, req: Request<State>, next: Next<'_, State>) -> Result {
-        HTTP_REQUESTS_TOTAL.inc();
-
-        let start = Instant::now();
+        HTTP_REQUESTS_TOTAL.with_label_values(&[req.url().path()]).inc();
         CONCURRENT_CONNECTIONS.inc();
 
-        let r = next.run(req).await;
-
-        CONCURRENT_CONNECTIONS.dec();
+        let start = Instant::now();
+        let response = next.run(req).await;
         let duration = start.elapsed();
 
-        HTTP_REQUEST_LATENCY.observe(duration.as_secs_f64());
+        CONCURRENT_CONNECTIONS.dec();
 
-        Ok(r)
+        let duration_in_seconds =
+            duration.as_secs_f64() + f64::from(duration.subsec_nanos()) / 1_000_000_000.0;
+        HTTP_REQUEST_LATENCY.observe(duration_in_seconds);
+
+        if response.error().is_some() {
+            ERROR_COUNT.inc();
+        }
+
+        Ok(response)
     }
 }
