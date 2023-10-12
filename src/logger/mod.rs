fafa08bf1d8c4b12107b4afe9e25eff1ccfc6ac7
Lucas Oliveira <olivmath@protonmail.com>
2023-10-12 19:01:42 -0300

    update: convert function into macro

--- src/logger/mod.rs
+++ src/logger/mod.rs
@@ -1,16 +1,20 @@
-use crate::metrics::constants::ERROR_COUNT;
-use log::error;
-use std::fmt::Display;
-
 pub fn init_logger() {
     log4rs::init_file("wess.yaml", Default::default()).unwrap();
 }
 
-pub fn log_error<E>(e: E) -> E
-where
-    E: Display,
-{
-    error!(target: "wess::err", "{e}");
-    ERROR_COUNT.inc();
-    e
+macro_rules! log_error {
+    ($err:expr) => {
+        {
+            use crate::metrics::constants::ERROR_COUNT;
+            use log::error;
+
+            ERROR_COUNT.inc();
+
+            let e = $err;
+            let e_str = e.to_string();
+
+            error!(target: "wess::err", "{}", e_str);
+            e
+        }
+    };
 }
