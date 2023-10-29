da3f740dd76f75b80c664a9d47103ab24eea29b1
Lucas Oliveira <olivmath@protonmail.com>
2023-10-29 12:00:03 -0300

    update(wess/server/response): respond function to receive http status code, and add todo

--- src/server/response.rs
+++ src/server/response.rs
@@ -1,8 +1,21 @@
+use crate::errors::WessError;
 use serde::Serialize;
 use serde_json::json;
 use tide::{Error, Response, StatusCode};
 
-use crate::errors::WessError;
+/// TODO
+/// join this functions into a module called Responder
+/// put this new module such as a deps to pass into `performers_ops`
+///
+/// # example:
+/// ```rust
+/// pub async fn make_run_op(mut req: Request<AppState>, response: Responder) -> Result<Response, Error> {
+///     match result {
+///         Ok(data) => response.success(data).await
+///         Err(err) => response.fail(err).await
+///     }
+/// }
+/// ```
 
 /// # Returns an error response with the provided message.
 ///
@@ -28,11 +41,11 @@ pub async fn respond_with_error(error: WessError) -> Result<Response, Error> {
 /// ## Returns
 ///
 /// * A [`Result`] containing the successful response.
-pub async fn respond<T>(message: T) -> Result<Response, Error>
+pub async fn respond<T>(message: T, status: StatusCode) -> Result<Response, Error>
 where
     T: Serialize,
 {
-    Ok(Response::builder(StatusCode::Ok)
+    Ok(Response::builder(status)
         .body(json!({ "message": message }))
         .build())
 }
