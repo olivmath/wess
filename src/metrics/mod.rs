0685562f8d60b7034c0e8fea850d75b54ba0ea02
Lucas Oliveira <olivmath@protonmail.com>
2024-08-19 02:40:08 -0300

    add: version in metrics

--- src/metrics/mod.rs
+++ src/metrics/mod.rs
@@ -1,15 +1,18 @@
 pub mod constants;
 
-use self::constants::{CPU_USAGE, DATABASE_SIZE, MEMORY_USAGE};
+use self::constants::{CPU_USAGE, DATABASE_SIZE, MEMORY_USAGE, APP_VERSION};
 use crate::database::DB;
 use std::{sync::Arc, time::Duration};
-use sysinfo::{System, get_current_pid};
+use sysinfo::{get_current_pid, System};
 
 pub async fn collect_usage_metrics() {
+    let version = env!("CARGO_PKG_VERSION");
+    APP_VERSION.with_label_values(&[version]).set(1);
+
     loop {
         let mut sys = System::new();
         sys.refresh_all();
-        
+
         MEMORY_USAGE.set(sys.used_memory() as i64);
         if let Ok(pid) = get_current_pid() {
             CPU_USAGE.set(sys.process(pid).unwrap().cpu_usage() as i64);
@@ -22,7 +25,6 @@ pub async fn collect_usage_metrics() {
             .property_int_value("rocksdb.estimate-live-data-size")
             .unwrap()
             .unwrap() as i64;
-
 
         DATABASE_SIZE.set(db_size);
         tokio::time::sleep(Duration::from_secs(5)).await;
